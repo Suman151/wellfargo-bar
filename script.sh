3a7a44d4b9a812ed8508e2ace23002fd96b9285b
Suman151 <51076949+Suman151@users.noreply.github.com>
2019-05-27 14:59:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,7 @@ ls -ltr
 echo "testing" >> test
 ls -ltr
 echo "$Environment for $Variable" >> test
+
 echo "[INFO] GIT Jenkins Integratioon Demo"
 echo "[INFO] POLL SCM Demo"
-echo "[INFO BRANCH Demo"
+echo "[INFO] BRANCH Demo"
